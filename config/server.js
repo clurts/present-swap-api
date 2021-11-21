878233bca62385126178fbfcb8da13baf31872aa
Carsten Lund <clu@rts.dk>
2021-11-21 11:49:00 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9e7266aabc523ee407ac9c18a74f4149'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '557c587d-5050-40e9-8f90-b936f0561da6'
+};
