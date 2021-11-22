eb4eff44826aafada98f40539fd3f83b260be916
Carsten Lund <clu@rts.dk>
2021-11-22 08:56:41 +0100

    move secret to env variable

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '9e7266aabc523ee407ac9c18a74f4149'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
